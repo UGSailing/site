e9345694582ab8e3f856672ff2d583529dc7867a
liki-mc <liki.mc@outlook.com>
2025-07-29 16:05:43 +0200

    added material react table to render todo table

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,15 +1,88 @@
+'use client';
 import React from 'react';
 import Head from "next/head";
 
+import { useMemo } from 'react';
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+  type MRT_ColumnDef,
+} from 'material-react-table';
+
 import todos from '@/data/todo';
+import { statuses, priorities, type ToDo } from '@/data/todo';
 
 const Todos = () => {
+    const columns = useMemo<MRT_ColumnDef<ToDo, any>[]>(
+        () => [
+            {
+                accessorKey: 'title',
+                header: 'Title',
+                filterVariant: 'autocomplete',
+                size: 100,
+            },
+            {
+                accessorKey: 'completed',
+                header: 'Completed',
+                filterVariant: 'checkbox',
+                size: 50,
+            },
+            {
+                accessorKey: 'status.id',
+                header: 'Status',
+                filterVariant: 'multi-select',
+                filterSelectionOptions: statuses.map(status => ({
+                    value: status.id,
+                    label: status.name,
+                })),
+                Cell: ({ cell }) => {
+                    const status = statuses.find(s => s.id === cell.getValue<string>());
+                    return status ? (
+                        <span 
+                            style={{ color: status.color }}
+                        >
+                            {status.name}
+                            <span dangerouslySetInnerHTML={{ __html: status.symbol }} />
+                        </span>
+                    ) : null;
+                }
+            },
+            {
+                accessorKey: 'priority.id',
+                header: 'Priority',
+                filterVariant: 'multi-select',
+                filterSelectionOptions: priorities.map(priority => ({
+                    value: priority.id,
+                    label: priority.name,
+                })),
+                Cell: ({ cell }) => {
+                    const priority = priorities.find(p => p.id === cell.getValue<string>());
+                    return priority ? (
+                        <span 
+                            style={{ color: priority.color }}
+                        >
+                            {priority.name}
+                            <span dangerouslySetInnerHTML={{ __html: priority.symbol }} />
+                        </span>
+                    ) : null;
+                }
+            }
+        ], []
+    )
+
+    const table = useMaterialReactTable({
+        columns,
+        data: todos,
+        initialState: { showColumnFilters: true },
+    });
+
     return <>
         <Head>
             <title>Todos - UGent Sailing</title>
             <meta name="description" content="Todos of UGent Sailing" />
         </Head>
-        <div>
+        <MaterialReactTable table={table} />
+        {/* <div>
             {
                 todos?.map(todo => (
                     <div key={todo.id} className="todo-item">
@@ -51,7 +124,7 @@ const Todos = () => {
                     </div>
                 ))
             }
-        </div>
+        </div> */}
     </>;
 };
 
