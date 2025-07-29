cdaeaeb7bd223ee3ae7cf9fc228ba5a2ec823c2f
liki-mc <liki.mc@outlook.com>
2025-07-29 16:49:12 +0200

    can now filter on status and priority

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -20,6 +20,7 @@ const Todos = () => {
                 header: 'Title',
                 filterVariant: 'autocomplete',
                 size: 100,
+                enableColumnOrdering: false
             },
             {
                 accessorKey: 'completed',
@@ -31,7 +32,7 @@ const Todos = () => {
                 accessorKey: 'status.id',
                 header: 'Status',
                 filterVariant: 'multi-select',
-                filterSelectionOptions: statuses.map(status => ({
+                filterSelectOptions: statuses.map(status => ({
                     value: status.id,
                     label: status.name,
                 })),
@@ -51,7 +52,7 @@ const Todos = () => {
                 accessorKey: 'priority.id',
                 header: 'Priority',
                 filterVariant: 'multi-select',
-                filterSelectionOptions: priorities.map(priority => ({
+                filterSelectOptions: priorities.map(priority => ({
                     value: priority.id,
                     label: priority.name,
                 })),
@@ -74,6 +75,7 @@ const Todos = () => {
         columns,
         data: todos,
         initialState: { showColumnFilters: true },
+        enableColumnOrdering: true,
     });
 
     return <>
@@ -82,49 +84,7 @@ const Todos = () => {
             <meta name="description" content="Todos of UGent Sailing" />
         </Head>
         <MaterialReactTable table={table} />
-        {/* <div>
-            {
-                todos?.map(todo => (
-                    <div key={todo.id} className="todo-item">
-                        <h3>{todo.title}</h3>
-                        {
-                            todo.status ? (
-                                <p>
-                                    Status: 
-                                        <span 
-                                            style={{ color: todo.status.color }} 
-                                            dangerouslySetInnerHTML={{ __html: todo.status.symbol }}
-                                        ></span> 
-                                    <span style={{ color: todo.status.color }}>{todo.status.name}</span>
-                                </p>
-                            ) : (
-                                <p></p>
-                            )
-                        }
-                        {
-                            todo.priority ? (
-                                <p>
-                                    Priority: 
-                                        <span 
-                                            style={{ color: todo.priority.color }} 
-                                            dangerouslySetInnerHTML={{ __html: todo.priority.symbol }}
-                                        ></span> 
-                                    <span style={{ color: todo.priority.color }}>{todo.priority.name}</span>
-                                </p>
-                            ) : (
-                                <p></p>
-                            )
-                        }
-                        <p>Created At: {new Date(todo.createdAt).toLocaleDateString()}</p>
-                        <p>Updated At: {new Date(todo.updatedAt).toLocaleDateString()}</p>
-                        {todo.notes && <p>Notes: {todo.notes}</p>}
-                        {todo.teams && todo.teams.length > 0 && (
-                            <p>Teams: {todo.teams.map(team => team.name).join(', ')}</p>
-                        )}
-                    </div>
-                ))
-            }
-        </div> */}
+        
     </>;
 };
 
