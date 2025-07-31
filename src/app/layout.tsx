0ac3994f9bb8b582833984f26a0ea399b04a7678
liki-mc <liki.mc@outlook.com>
2025-07-31 22:18:12 +0200

    speciaal voor Jorien int rood

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { NavBar } from "@/components/navbar";
+import Banner from "@/components/banner";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -18,12 +19,17 @@ export default function RootLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const bannerData = {
+        color: "#ff0000",
+        message: "This website is under construction.",
+    }
     return (
         <html lang="en">
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
                 <NavBar />
+                <Banner {...bannerData} />
                 {children}
             </body>
         </html>
