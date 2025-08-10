d0e5faedc8fc51cae3de97a987883a331f69777e
liki-mc <liki.mc@outlook.com>
2025-08-10 14:01:55 +0200

    changed to use Sliders built in responsive design

--- src/components/sponsors.tsx
+++ src/components/sponsors.tsx
@@ -1,44 +1,47 @@
 "use client";
 import sponsors from "@/data/sponsors";
 import Link from "next/link";
-import { useState, useEffect } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export function Sponsors() {
-    const [visibleLogos, setVisibleLogos] = useState(4); // Default to 4 logos per view (fallback for SSR)
-
-    const logosPerView = {
-        sm: 2, // Small screens
-        md: 3, // Medium screens
-        lg: 4, // Large screens
-    };
-
-    // Determine the number of logos to show based on screen size
-    const getLogosPerView = () => {
-        if (window.innerWidth >= 1024) return logosPerView.lg; // Large screens
-        if (window.innerWidth >= 768) return logosPerView.md; // Medium screens
-        return logosPerView.sm; // Small screens
-    };
-
-    useEffect(() => {
-        const handleResize = () => {
-            setVisibleLogos(getLogosPerView());
-        };
-
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
+    const visibleLogos = sponsors.length
 
     const settings = {
         infinite: true,
-        slidesToShow: Math.min(visibleLogos, sponsors.length),
-        slidesToScroll: Math.min(visibleLogos, sponsors.length),
+        slidesToShow: Math.min(visibleLogos, 4),
+        slidesToScroll: Math.min(visibleLogos, 4),
         autoplay: visibleLogos < sponsors.length,
         speed: 1000,
         autoplaySpeed: 5000,
-        cssEase: "linear"
+        cssEase: "linear",
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: Math.min(visibleLogos, 3),
+                    slidesToScroll: Math.min(visibleLogos, 3),
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: Math.min(visibleLogos, 2),
+                    slidesToScroll: Math.min(visibleLogos, 2),
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     }
 
     return (
