a1b3511f49dbac34b23253e1a768454508d0ac6a
Cédric Van Cauwenberghe <Cedric.VanCauwenberghe@UGent.be>
2025-08-21 13:28:56 +0200

    feat: add titles to pages with Metadata-objects

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+    title: "Contact",
+    description: "Contact UGent Sailing",
+}
 const Contact = () => {
     return <div>Contact</div>;
 };
--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import {Metadata} from "next";
 
+export const metadata: Metadata = {
+    title: "Join",
+    description: "Join UGent Sailing",
+}
 const Join = () => {
     return <div>Join page</div>;
 };
