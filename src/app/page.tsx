b88382a73572cb1bc77690d891a0db5d1d8608cf
liki-mc <liki.mc@outlook.com>
2025-08-29 18:29:25 +0200

    added sections on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,10 +51,33 @@ export default function Home() {
                     }
                 </Slider>
             </div>
-            <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+            <div className="font-sans items-center justify-items-center min-h-screen px-6">
                 
-                <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-                    
+                <main className="flex flex-col gap-[32px] row-start-1 items-center sm:items-start">
+                    <section>
+                        <h2 className="text-3xl font-bold mb-4">
+                            Intro
+                        </h2>
+                        Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+                    </section>
+                    <section>
+                        <h2 className="text-3xl font-bold mb-4">
+                            Calender
+                        </h2>
+                        To be implemented
+                    </section>
+                    <section>
+                        <h2 className="text-3xl font-bold mb-4">
+                            News
+                        </h2>
+                        To be implemented
+                    </section>
+                    <section>
+                        <h2 className="text-3xl font-bold mb-4">
+                            Board
+                        </h2>
+                        To be implemented
+                    </section>
                 </main>
             </div>
         </div>
