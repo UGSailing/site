472a0e7bb665ad55b254f6ec7713835c65bc394a
liki-mc <liki.mc@outlook.com>
2025-08-29 19:20:41 +0200

    created partner page

---
+++ src/app/partners/page.tsx
@@ -0,0 +1,30 @@
+import { Alert, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import {Metadata} from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+    title: "Partners",
+    description: "Partners - UGent Sailing",
+}
+const Partners = () => {
+    return (
+        <>
+            <div className="font-sans items-center justify-items-center min-h-screen px-6 mt-5">
+                <main className="flex flex-col gap-[32px] row-start-1 items-center sm:items-start">
+                    <Link href="/contact" className="w-full text-center">
+                        <Alert className="w-full">
+                            <AlertTitle>
+                                Want to become a partner? <span className="icon-[bi--arrow-right] h-3"></span> Contact us!
+                            </AlertTitle>
+                        </Alert>
+                    </Link>
+                    
+                    To be implemented
+                </main>
+            </div>
+        </>
+    );
+};
+
+export default Partners;
