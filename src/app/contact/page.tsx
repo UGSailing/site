a428733d883a83ad63ba133cbf3cbfa1e0a1c26c
liki-mc <liki.mc@outlook.com>
2025-08-29 17:16:19 +0200

    added shadcn for components, choice of Jorien and Rode Thomas

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,14 @@
 import { Metadata } from "next";
+import { Button } from "@/components/ui/button";
 
 export const metadata: Metadata = {
     title: "Contact",
     description: "Contact UGent Sailing",
 }
 const Contact = () => {
-    return <div>Contact</div>;
+    return <div>Contact
+        <Button>Click me</Button>
+    </div>;
 };
 
 export default Contact;
