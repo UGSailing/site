ebaea3ce4fc86fc2e896524cabd31030d8f95c53
liki-mc <liki.mc@outlook.com>
2025-09-04 16:00:32 +0200

    added footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,15 +1,15 @@
 import Link from "next/link";
+import { H4 } from ".";
+import socials from "./socials";
 
 export function Footer() {
     return (
-        <div className="bg-red-800 px-5 md:px-10 lg:px-15 2xl:px-20 mt-5 py-5 text-white">
+        <div className="bg-red px-5 md:px-10 lg:px-15 2xl:px-20 mt-5 py-5 text-white">
             <div className="w-full grid grid-cols-1 md:grid-cols-2">
                 <div className="text-sm m-4">
                     {/* Temp div */}
                     <div>
-                        <p className="mb-2">
-                            <strong>UGent Sailing</strong>
-                        </p>
+                        <H4 className="text-white mb-2">UGent Sailing</H4>
                         <p>Follow us on instagram to receive the latest information</p>
                     </div>
                     {/* Final div */}
@@ -26,68 +26,25 @@ export function Footer() {
                     </div>
                 </div>
                 <div>
-                    <div className="m-4">
+                    <div className="text-sm m-4">
+                        <H4 className="text-white mb-2">Contact us</H4>
+                        <p className="mb-2">Via mail: <a href="mailto:contact@ugentsailing.be"><u>contact@ugentsailing.be</u></a></p>
                         <ul className="flex font-medium">
-                            <li className="mx-3">
-                                <Link 
-                                    className="nav-link" 
-                                    href="https://www.facebook.com/people/UGent-Sailing/61573219499660/" 
-                                    target="_blank" 
-                                    rel="noopener noreferrer"
-                                >
-                                    <i className="icon-[bi--facebook]"></i>
-                                </Link>
-                            </li>
-                            <li className="mx-3">
-                                <Link 
-                                    className="nav-link" 
-                                    href="https://www.instagram.com/ugentsailing/" 
-                                    target="_blank" 
-                                    rel="noopener noreferrer"
-                                >
-                                    <i className="icon-[bi--instagram]"></i>
-                                </Link>
-                            </li>
-                            <li className="mx-3">
-                                <Link 
-                                    className="nav-link" 
-                                    href="https://discord.gg/BmRtd6qb4z" 
-                                    target="_blank" 
-                                    rel="noopener noreferrer"
-                                >
-                                    <i className="icon-[bi--discord]"></i>
-                                </Link>
-                            </li>
-                            <li className="mx-3">
-                                <Link 
-                                    className="nav-link" 
-                                    href="https://www.linkedin.com/company/ugent-sailing/posts/?feedView=all" 
-                                    target="_blank" 
-                                    rel="noopener noreferrer"
-                                >
-                                    <i className="icon-[bi--linkedin]"></i>
-                                </Link>
-                            </li>
-                            <li className="mx-3">
-                                <Link 
-                                    className="nav-link" 
-                                    href="https://github.com/UGSailing" 
-                                    target="_blank" 
-                                    rel="noopener noreferrer"
-                                >
-                                    <i className="icon-[bi--github]"></i>
-                                </Link>
-                            </li>
+                            {
+                                socials.map((social, i) => (
+                                    <li key={i} className="mx-3">
+                                        <Link 
+                                            className="nav-link" 
+                                            href={social.href} 
+                                            target="_blank" 
+                                            rel="noopener noreferrer"
+                                        >
+                                            <i className={social.icon}></i>
+                                        </Link>
+                                    </li>
+                                ))
+                            }
                         </ul>
-                    </div>
-                    <div>
-                        <u>
-                            <Link
-                                href="/contact"
-                            >
-                                Contact
-                            </Link>
-                        </u>
                     </div>
                 </div>
             </div>
