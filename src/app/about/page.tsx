0cb53f5dd62c24f7990cd86359e7947a1eeae280
liki-mc <liki.mc@outlook.com>
2025-09-08 01:00:59 +0200

    added content to about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import { H2, Quote } from "@/components";
+import { A, H2, H4, Quote } from "@/components";
 
 export const metadata: Metadata = {
     title: "About",
@@ -14,17 +14,47 @@ const About = () => {
                 <section>
                     <H2>Info</H2>
 
-                    Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+                    <H4>Who We Are</H4>
+                    UGent Sailing is a team of bachelor’s, master’s, and PhD students, as well as young graduates. 
+                    Everyone under 27 at the start of a work year is welcome to join, regardless of background.
+                    At least half of our members are UGent students.
+
+                    <H4>What We Do</H4>
+                    We are building an autonomous, CO₂-neutral vessel to compete in the <A href="https://energyboatchallenge.com/">Monaco Energy Boat Challenge</A> , 
+                    a prestigious international competition. Our first participation will be in July 2026 in the AI class, 
+                    but we will also participated in the Sealab class from July 2027 on.
+
+                    <H4>Our Goals</H4>
+                    As a student association, we pursue three main goals: (check dit even voor betere voorwaarden in onze statuten pls, die vind je online in UBOregister ofz)
+                    <ol className="list-decimal list-inside">
+                        <li>
+                            Bringing together people with a passion for maritime engineering, technology, and automation.
+                        </li>
+                        <li>
+                            Offering young people a hands-on learning environment.
+                        </li>
+                        <li>
+                            Contributing to a more sustainable future.
+                        </li>
+                    </ol>
+
+                    <H4>How We Work</H4>
+
+                    We meet every Monday to keep the entire team up to date. Our team is built on a horizontal structure where every voice matters equally, 
+                    and each member takes responsibility for a part of the project they choose at the start. 
+                    In addition, we organize design sprints every one to two weeks, open to all members who want to contribute and learn together. 
+                    Our locals are at the Technology Park, in Zwijnaarde. 
+
                 </section>
                 <section>
                     <H2>History</H2>
 
-                    Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+                    Coming soon
                 </section>
                 <section>
-                    <H2>Woordje van het team</H2>
+                    <H2>From the team</H2>
 
-                    Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+                    Coming soon
                     <br />
                     <Quote>
                         &quot;A fancy quote from Jorien&quot;
