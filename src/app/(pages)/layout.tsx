f469302fa2ac7485bee3fe08975786a540ad772e
liki-mc <liki.mc@outlook.com>
2025-10-26 17:44:19 +0100

    created form logic

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
             >
                 <NavBar />
                 {/* <Banner {...bannerData} /> */}
-                {children}
+                { children }
                 <Partners />
                 <Footer />
             </body>
