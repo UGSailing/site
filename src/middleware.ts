83b836b77404ce10a1ffaf03911aa5089aa40254
NuttyShrimp <jan.lecoutere@hotmail.be>
2025-10-27 18:33:27 +0100

    feat(admin): secure admin pages from unauthorized users

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,17 @@
-export { auth as middleware } from '@/lib/auth'
+import { NextRequest, NextResponse } from 'next/server'
+
+import { auth } from '@/lib/auth'
+
+export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith('/admin')) {
+    const resp = await auth();
+    if (!resp) {
+      return NextResponse.redirect(new URL('/login', request.url))
+    }
+  }
+}
 
 export const config = {
-    runtime: 'nodejs',
+  runtime: 'nodejs',
 }
+
