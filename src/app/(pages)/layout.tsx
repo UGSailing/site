66b9a5396ae0ffc4a2b7c4cb7d68c25136ea5ae4
mevergot <Mert.Vergote@UGent.be>
2025-10-27 23:18:11 +0100

    Added fancy custom 404 page. Some layout.tsx elements had to be moved to root. Otherwise the 404 page couldnt acces them : D

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { NavBar } from "@/components/navbar";
 import { Partners } from "@/components/partners";
 import Banner from "@/components/banner";
 import { Footer } from "@/components/footer";
-
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
     title: {
@@ -23,8 +12,7 @@ export const metadata: Metadata = {
     }
 }
 
-
-export default function RootLayout({
+export default function PagesLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
@@ -35,16 +23,12 @@ export default function RootLayout({
         textColor: "#111111"
     }
     return (
-        <html lang="en">
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
-                <NavBar />
-                {/* <Banner {...bannerData} /> */}
-                { children }
-                <Partners />
-                <Footer />
-            </body>
-        </html>
+        <>
+            <NavBar />
+            {/* <Banner {...bannerData} /> */}
+            {children}
+            <Partners />
+            <Footer />
+        </>
     );
 }
