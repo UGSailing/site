0861aaf3f154ef8cef0a1eb35b82832d0f5fca59
liki-mc <liki.mc@outlook.com>
2025-10-27 22:14:09 +0100

    ran lint fix

--- src/app/(pages)/admin/layout.tsx
+++ src/app/(pages)/admin/layout.tsx
@@ -4,20 +4,20 @@ import { SessionProvider } from "next-auth/react";
 import { redirect, RedirectType } from "next/navigation";
 
 export default async function AdminLayout({ children }: PropsWithChildren) {
-  const session = await auth()
-  if (session?.user) {
-    session.user = {
-      name: session.user.name,
-      email: session.user.email,
-      image: session.user.image,
+    const session = await auth()
+    if (session?.user) {
+        session.user = {
+            name: session.user.name,
+            email: session.user.email,
+            image: session.user.image,
+        }
+    } else {
+        return redirect("/login", RedirectType.replace);
     }
-  } else {
-    return redirect("/login", RedirectType.replace);
-  }
 
-  return (
-    <SessionProvider session={session}>
-      {children}
-    </SessionProvider>
-  )
+    return (
+        <SessionProvider session={session}>
+            {children}
+        </SessionProvider>
+    )
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,15 +3,15 @@ import { NextRequest, NextResponse } from 'next/server'
 import { auth } from '@/lib/auth'
 
 export async function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith('/admin')) {
-    const resp = await auth();
-    if (!resp) {
-      return NextResponse.redirect(new URL('/login', request.url))
+    if (request.nextUrl.pathname.startsWith('/admin')) {
+        const resp = await auth();
+        if (!resp) {
+            return NextResponse.redirect(new URL('/login', request.url))
+        }
     }
-  }
 }
 
 export const config = {
-  runtime: 'nodejs',
+    runtime: 'nodejs',
 }
 
