6540f86a35a5aefbc7a4833a110fab8ad485c647
liki-mc <liki.mc@outlook.com>
2025-11-26 01:56:10 +0100

    added custom discord login

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,6 +8,43 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     adapter: PrismaAdapter(prisma),
     providers: [
         GitHub,
-        Discord
+        Discord(
+            {
+                id: 'discord',
+                userinfo: `https://discord.com/api/users/@me/guilds/${process.env.GUILD_ID}/member`,
+                async profile(profile) {
+                    console.log("running custom implementation \n\n\n\n\n\n");
+                    console.log(profile);
+                    if (profile.avatar === null) {
+                        const defaultAvatarNumber =
+                            profile.user.discriminator === "0"
+                                ? Number(BigInt(profile.user.id) >> BigInt(22)) % 6
+                                : parseInt(profile.user.discriminator) % 5
+                        profile.image_url = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png`
+                    } else {
+                        const format = profile.avatar.startsWith("a_") ? "gif" : "png"
+                        profile.image_url = `https://cdn.discordapp.com/avatars/${profile.user.id}/${profile.user.avatar}.${format}`
+                    }
+                    // Ugly fix ma bon
+                    const roles = (await prisma.role.findMany()).map(role => role.id.toString());
+                    profile.valid_roles = profile.roles.filter((role_id: string) => roles.includes(role_id));
+
+                    return {
+                        id: profile.user.id,
+                        name: profile.nick ?? profile.user.global_name ?? profile.user.username,
+                        email: profile.user.email,
+                        image: profile.image_url,
+                        roles: {
+                            createMany: {
+                                data: profile.valid_roles.map((role_id : string) => ({ roleId : BigInt(role_id) })),
+                            }
+                        },
+                    }
+                },
+                authorization: {
+                    params: { scope: "guilds.members.read identify email" },
+                },
+            }
+        )
     ],
 });
