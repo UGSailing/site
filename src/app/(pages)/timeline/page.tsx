7e4e8e0611b35c1e59eb41d15462344d889bf038
liki-mc <liki.mc@outlook.com>
2025-12-05 17:27:25 +0100

    timeline events are dynamically

--- src/app/(pages)/timeline/page.tsx
+++ src/app/(pages)/timeline/page.tsx
@@ -1,39 +1,82 @@
 import Carousel, { CarouselItem } from "@/components/carousel";
 import { Metadata } from "next";
-import { events } from "@/data/events";
-import { EventCard } from "@/components/eventCard";
+import { EventCard, FutureEventCard } from "@/components/eventCard";
 import { generations } from "@/data/generation";
 import { GenerationCard } from "@/components/genCard";
 import { H1 } from "@/components";
 import { parse } from 'date-fns';
+import prisma from "@/prisma";
 
 export const metadata: Metadata = {
     title: "Timeline",
     description: "Timeline of UGent Sailing",
 }
 
-function getNextEventIndex(events: typeof import('@/data/events').events) {
+type Event = {
+    image: {
+        id: string;
+        createdAt: Date;
+        updatedAt: Date;
+        filename: string;
+        filepath: string;
+        mimetype: string;
+        size: number;
+        width: number | null;
+        height: number | null;
+        uploadedById: string | null;
+    } | null;
+} & {
+    endDate: Date;
+    startDate: Date;
+    id: number;
+    createdAt: Date;
+    updatedAt: Date;
+    title: string;
+    location: string;
+    intro: string;
+    description: string | null;
+    registration: number | null;
+    imageId: string | null;
+}
+
+function getNextEventIndex(events: Event[]) {
     const now = new Date();
-    const dates = events.map(event => parse(event.endDate ?? event.startDate, 'dd-MM-yyyy', new Date()));
+    const dates = events.map(event => event.endDate ?? event.startDate);
     const deltas = dates.map(date => date.getTime() - now.getTime());
     // index of smallest delta
-    const nextindex =  deltas.indexOf(Math.min(...deltas.filter(d => d >= 0))) || -1;
-    if(nextindex !== -1) return nextindex;
+    const nextindex = deltas.indexOf(Math.min(...deltas.filter(d => d >= 0))) || -1;
+    if (nextindex !== -1) return nextindex;
     else return Math.max(deltas.length - 1, 0); // default to last if no upcoming event
 }
 
-const Timeline = () => {
-    
+const Timeline = async () => {
+    const events = await prisma.event.findMany({
+        orderBy: {
+            startDate: 'asc',
+        },
+        include: {
+            image: true,
+        }
+    });
     return (
         <div className="mt-5 font-sans items-center justify-items-center min-h-screen px-6">
             <main className="w-full flex flex-col gap-[32px] row-start-1 items-center sm:items-start">
                 <H1 className="text-center w-full">Events</H1>
-                <Carousel startIndex={getNextEventIndex(events)} buttonSettings={ { size: "xl" } } key="timeline-carousel">
-                    { events.map((event) => <CarouselItem className="md:basis-1/3" key={event.id}><EventCard event={event}></EventCard></CarouselItem>) }
+                <Carousel startIndex={getNextEventIndex(events)} buttonSettings={{ size: "xl" }} key="timeline-carousel">
+                    {
+                        events.map((event) => (
+                            <CarouselItem className="md:basis-1/3" key={event.id}>
+                                <EventCard event={event}></EventCard>
+                            </CarouselItem>)
+                        )
+                    }
+                    <CarouselItem className="md:basis-1/3" key="future-events">
+                        <FutureEventCard></FutureEventCard>
+                    </CarouselItem>)
                 </Carousel>
                 <H1 className="text-center w-full">Our Ship</H1>
                 <Carousel buttonSettings={{ size: "xl" }} key="ship-carousel">
-                    { generations.map((generation) => <CarouselItem className="md:basis-1/3" key={generation.name}><GenerationCard generation={generation}></GenerationCard></CarouselItem>) }
+                    {generations.map((generation) => <CarouselItem className="md:basis-1/3" key={generation.name}><GenerationCard generation={generation}></GenerationCard></CarouselItem>)}
                 </Carousel>
             </main>
         </div>
--- src/components/eventCard.tsx
+++ src/components/eventCard.tsx
@@ -6,11 +6,37 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
-import { type EventType } from "@/data/events";
 import { Button } from "./ui/button";
 
-export function EventCard({ event }: { event: EventType }) {
-    const eventPassed = Date.parse(event.startDate) < Date.now() + 1.5 * 3600 * 1000;
+type Event = {
+    image: {
+        id: string;
+        createdAt: Date;
+        updatedAt: Date;
+        filename: string;
+        filepath: string;
+        mimetype: string;
+        size: number;
+        width: number | null;
+        height: number | null;
+        uploadedById: string | null;
+    } | null;
+} & {
+    endDate: Date;
+    startDate: Date;
+    id: number;
+    createdAt: Date;
+    updatedAt: Date;
+    title: string;
+    location: string;
+    intro: string;
+    description: string | null;
+    registration: number | null;
+    imageId: string | null;
+}
+
+export function EventCard({ event }: { event: Event }) {
+    const eventPassed = event.startDate.getTime() < Date.now() + 1.5 * 3600 * 1000;
     const disabled = { disabled: eventPassed || !event.registration };
     return (
         <div className="w-full grid grid-flow-col py-1 justify-items-center">
@@ -20,7 +46,7 @@ export function EventCard({ event }: { event: EventType }) {
                         { event.title }
                     </CardTitle>
                     <CardDescription>
-                        <p className="text-sm text-gray-600 mb-1"><span className="bg-gray-600 icon-[material-symbols--calendar-today]"></span>{event.startDate} {event.endDate ? `- ${event.endDate}` : ""}</p>
+                        <p className="text-sm text-gray-600 mb-1"><span className="bg-gray-600 icon-[material-symbols--calendar-today]"></span>{event.startDate.toLocaleDateString()} {event.endDate ? `- ${event.endDate.toLocaleDateString()}` : ""}</p>
                         <p className="text-sm text-gray-600 h-8"><span className="bg-gray-600 icon-[material-symbols--location-on]"></span>{event.location}</p>
                     </CardDescription>
                     <CardAction>
@@ -30,9 +56,35 @@ export function EventCard({ event }: { event: EventType }) {
                     </CardAction>
                 </CardHeader>
                 <CardContent>
-                    <img src={event.image} alt={event.title + " image"} className={`mb-4 rounded-md ${eventPassed ? "grayscale" : ""}`} />
+                    <img src={event.image?.filepath || "/img/logos/cropped_logo.png"} alt={event.title + " image"} className={`mb-4 rounded-md ${eventPassed ? "grayscale" : ""}`} />
                 </CardContent>
             </Card>
         </div>
     );
 };
+
+export function FutureEventCard() {
+    return (
+        <div className="w-full grid grid-flow-col py-1 justify-items-center">
+            <Card className={`w-full max-w-sm border-red-500`}>
+                <CardHeader>
+                    <CardTitle className={`text-red-700 text-lg`}>
+                        Future events coming soon!
+                    </CardTitle>
+                    <CardDescription>
+                        <p className="text-sm text-gray-600 mb-1"><span className="bg-gray-600 icon-[material-symbols--calendar-today]"></span>To be announced</p>
+                        <p className="text-sm text-gray-600 h-8"><span className="bg-gray-600 icon-[material-symbols--location-on]"></span>To be announced</p>
+                    </CardDescription>
+                    <CardAction>
+                        <Button disabled variant="secondary" size="sm" className={`mt-2 text-white bg-red-500`}>
+                            Register
+                        </Button>
+                    </CardAction>
+                </CardHeader>
+                <CardContent>
+                    <img src={"/img/logos/cropped_logo.png"} alt={"Future events image"} className={`mb-4 rounded-md`} />
+                </CardContent>
+            </Card>
+        </div>
+    );
+}
