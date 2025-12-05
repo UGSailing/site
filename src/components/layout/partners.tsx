50d59e2a27e1a43ebd7f48901c30199bf82e24ad
liki-mc <liki.mc@outlook.com>
2025-12-05 16:35:48 +0100

    removed depricated import

--- src/components/layout/partners.tsx
+++ src/components/layout/partners.tsx
@@ -1,13 +1,6 @@
 "use server";
-import { ApiTypes } from "@/prisma/apiclient";
 import prisma from "@/prisma";
 import { PartnerCarousel } from "./partnerCarousel";
-
-type ExtendedPartner = ApiTypes["Partner"] & {
-    attributes: ApiTypes["Partner"]["attributes"] & {
-        logo?: ApiTypes["Media"] | null;
-    }
-};
 
 export async function Partners() {
     const partners = await prisma.partner.findMany({
