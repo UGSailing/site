45b4a6674f042041492025f4b6110f80da990276
liki-mc <liki.mc@outlook.com>
2025-12-10 22:54:50 +0000

    added line breaks

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -15,13 +15,17 @@ export default function Home() {
                         <H2>Intro</H2>
 
 UGent Sailing brings together students and young researchers with an interest in maritime technology, artificial intelligence, and innovation. Our goal is to create an environment where everyone can gain hands-on experience and grow, both technically and personally. To achieve this, we work with Design Sprints: open sessions held about once a week, where anyone can join to focus collectively on a specific part of the boat.
-
+                        <br/>
+                        <br/>
 Our broader mission is to contribute to a sustainable future for the maritime sector. We aim to show that sustainable and autonomous solutions to real-life problems go hand in hand; not as concepts of the future, but as technologies we can build today.
-
+                        <br/>
+                        <br/>
 To put our ideas into practice, we are competing in the Monaco Energy Boat Challenge. This competition gives us a shared goal to work towards and a tangible way to measure our progress. Alongside this main project, we also take on smaller side quests: developing a living lab to monitor the condition of the campus pond, and exploring the use of biofuels for future prototypes.
-
+                        <br/>
+                        <br/>
 This year, we are competing in the AI class, but our ambition reaches further. In the coming years, we aim to join the Sealab class, the research-focused master class of the competition. To get there, we are building strong connections with the university through VOP collaborations and master’s theses, ensuring our project grows to a research-worthy level.
-
+                        <br/>
+                        <br/>
 While UGent Sailing is primarily a student team, our community is open to everyone — from bachelor’s and master’s students to PhD candidates and young graduates. Together, we are working towards innovation on the water and a smarter, cleaner maritime future.
 
                     </section>
