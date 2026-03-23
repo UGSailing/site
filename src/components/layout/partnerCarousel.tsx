3cb3d695070d9d1027432e2acbda0ec8fdf11a6e
liki-mc <liki.mc@outlook.com>
2026-03-23 15:20:54 +0100

    added head partners to bottom partner bar

--- src/components/layout/partnerCarousel.tsx
+++ src/components/layout/partnerCarousel.tsx
@@ -28,34 +28,55 @@ type ExtendedPartner ={
     logoId: string | null;
 }
 
+export function HeadPartner({ partners }: { partners?: ExtendedPartner[] }) {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+            {
+                partners?.map((partner) => (
+                    <Link 
+                        key={partner.name}
+                        href={partner.url} 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                        className="flex flex-col items-center"
+                    >
+                        <img 
+                            src={partner.logo?.filepath} 
+                            alt={`${partner.name} logo`} 
+                            className="max-h-30 max-w-96 object-contain" 
+                        />
+                    </Link>
+                ))
+            }
+        </div>
+    );
+}
+
 export function PartnerCarousel({ partners }: { partners?: ExtendedPartner[] }) {
     return (
-        <div className="mx-6">
-            <H2 className="mb-6 pt-10">Our Partners</H2>
-            <div className="h-18 slider-container justify-center">
-                <Carousel opts={{ loop: true }} plugins={[Autoplay({ delay: 4000 })]}>
-                    {
-                        partners?.map((partner) => (
-                            <CarouselItem key={partner.name} className={"basis-full sm:basis-1/2" + (partners?.length > 2 ? " md:basis-1/3" : "") + (partners?.length > 3 ? " lg:basis-1/4" : "") + (partners?.length > 5 ? " xl:basis-1/5" : "")}>
-                                <Link 
-                                    href={partner.url} 
-                                    target="_blank" 
-                                    rel="noopener noreferrer"
-                                    className="flex flex-col items-center w-full"
-                                >
-                                    <div className="flex flex-wrap">
-                                        <img 
-                                            src={ partner.logo?.filepath } 
-                                            alt={`${partner.name} logo`} 
-                                            className="max-h-18 w-full object-contain" 
-                                        />
-                                    </div>
-                                </Link>
-                            </CarouselItem>
-                        ))
-                    }
-                </Carousel>
-            </div>
+        <div className="h-18 slider-container justify-center">
+            <Carousel opts={{ loop: true }} plugins={[Autoplay({ delay: 4000 })]}>
+                {
+                    partners?.map((partner) => (
+                        <CarouselItem key={partner.name} className={"basis-full sm:basis-1/2" + (partners?.length > 2 ? " md:basis-1/3" : "") + (partners?.length > 3 ? " lg:basis-1/4" : "") + (partners?.length > 5 ? " xl:basis-1/5" : "")}>
+                            <Link 
+                                href={partner.url} 
+                                target="_blank" 
+                                rel="noopener noreferrer"
+                                className="flex flex-col items-center w-full"
+                            >
+                                <div className="flex flex-wrap">
+                                    <img 
+                                        src={ partner.logo?.filepath } 
+                                        alt={`${partner.name} logo`} 
+                                        className="max-h-18 w-full object-contain" 
+                                    />
+                                </div>
+                            </Link>
+                        </CarouselItem>
+                    ))
+                }
+            </Carousel>
         </div>
     )
 }
--- src/components/layout/partners.tsx
+++ src/components/layout/partners.tsx
@@ -1,6 +1,7 @@
 import prisma from "@/prisma";
-import { PartnerCarousel } from "./partnerCarousel";
+import { PartnerCarousel, HeadPartner } from "./partnerCarousel";
 import { connection } from "next/server";
+import { H2, H4 } from "..";
 
 export async function Partners() {
     // Render at incoming request
@@ -16,6 +17,12 @@ export async function Partners() {
     });
 
     return (
-        <PartnerCarousel partners={partners} />
+        <div className="mx-6">
+            <H2 className="mb-6 pt-10">Our Partners</H2>
+            <H4>Head Partners</H4>
+            <HeadPartner partners={partners.filter((p) => p.isHead)} />
+            <H4 className="mt-6">Partners</H4>
+            <PartnerCarousel partners={partners.filter((p) => !p.isHead)} />
+        </div>
     )
 }
